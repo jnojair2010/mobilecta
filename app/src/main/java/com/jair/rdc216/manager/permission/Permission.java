958be82550ff3934f9937c9582a930ddc6350aad
JAIR DO NASCIMENTO DE OLIVEIRA <jnojair2010@gmail.com>
2023-11-27 19:05:15 -0300

    created permission multiple

--- app/src/main/java/com/jair/rdc216/manager/permission/Permission.java
+++ app/src/main/java/com/jair/rdc216/manager/permission/Permission.java
@@ -12,26 +12,94 @@ import android.util.Log;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Permission {
 
     private static int permissaoVerificado = 2;
 
     private static boolean jaAutorizouAoMenosUmaVez = false;
+    private static List<String> listaPermissao = new ArrayList<>();
+
+    private static  List<String> listaPermissoesNegada = new ArrayList<>();
 
 
     public static boolean validarPermission(int requestCode, Activity activity, String[] permission){
 
-        verificaPermissão(activity);
+        verificarPermissaoMultipla(activity, permission);
+        verificarPermissoesNegadaMultipla(activity, permission);
+        solicitarPermissoesMultiplas(activity, permission);
+        solicitarPermissaoNegadoMUltipla(activity, permission);
 
-        VerificaPermissaoAutorizadoOuNegado(activity);
-        solicitarPermissaoPelaPrimeiraVez(activity);
-        solicitarPermissaoSeNegado(activity);
+
 
         return true;
     }
 
+    private static boolean verificarPermissaoMultipla(Activity activity, String[] permission){
+
+        //for que percorre as permissoes passas e verifica se as tem permissoes concedida pelo usuario
+        for(String permissao : permission){
+            Boolean checkPermission = ContextCompat.checkSelfPermission(activity, permissao) == PackageManager.PERMISSION_GRANTED;
+
+            // verifica se a permissoa no não já foi concedida, as que foram concedida não é enviada a arraylist
+            if(checkPermission == false){
+
+                //envia cara permissoes para a ArrayList
+                listaPermissao.add(permissao);
+                Log.i("/////permission/////", " Verificação se já existe uma permissão: "+permissao+ " mais a scheckagem "+checkPermission);
+            }
+
+        };
+        return true;
+    }
+    private static boolean verificarPermissoesNegadaMultipla(Activity activity, String[] permission){
+
+        for(String permissao :permission){
+                ActivityCompat.shouldShowRequestPermissionRationale(activity, permissao);
+                jaAutorizouAoMenosUmaVez =  ActivityCompat.shouldShowRequestPermissionRationale(activity,permissao);
+                //Log.i("/////permission/////", " verificada as permissoes negada "+jaAutorizouAoMenosUmaVez);
+
+                if(jaAutorizouAoMenosUmaVez == true){
+                    listaPermissoesNegada.add(permissao);
+                        Log.i("/////permission/////", " verificada as permissoes negada "+jaAutorizouAoMenosUmaVez);
+                }
+        }
+
+        return true;
+    }
+    private static boolean solicitarPermissoesMultiplas(Activity activity, String[] permission){
+
+        //solicitar permissão pela primeira vez
+        for(String permissao : permission){
+            ActivityCompat.requestPermissions(activity,new String[]{permissao},0);
+            Log.i("/////permission/////", " permissoes solicitadas "+permissao);
+        }
+
+        return true;
+    }
+
+    private static boolean solicitarPermissaoNegadoMUltipla(Activity activity, String[] permission){
+
+        verificarPermissoesNegadaMultipla(activity, permission);
+
+            for(String permissoes : listaPermissoesNegada){
+                ActivityCompat.requestPermissions(activity,new String[]{permissoes},0);
+                Log.i("/////permission/////", " Solicitadas as permissoes que já foram negada "+permissoes);
+            }
+
+        return true;
+
+    }
+
+
+
+
+
     // verifica de exite permissoa
-    private static boolean verificaPermissão(Activity activity){
+    private static boolean verificaPermissãoIndividual(Activity activity){
               //verifica se ja existe uma permissão
              // se não está permitido retorna 0
         if(ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
@@ -42,7 +110,7 @@ public class Permission {
     return true;
     }
 
-    private static boolean VerificaPermissaoAutorizadoOuNegado(Activity activity){
+    private static boolean verificaPermissaoAutorizadoOuNegado(Activity activity){
 
         // verifica se o usuario já recebeu a notificação de permissão ao menus uma vez e ele já aceitou ou negou
         // se negou retorna true se permitiu retorna false
