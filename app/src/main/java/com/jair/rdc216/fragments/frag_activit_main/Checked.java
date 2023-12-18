80ec93e948a784e67161a431146a5c2d56c912c7
JAIR DO NASCIMENTO DE OLIVEIRA <jnojair2010@gmail.com>
2023-12-18 16:41:50 -0300

    tentativa do git action android 2

--- app/src/main/java/com/jair/rdc216/fragments/frag_activit_main/Checked.java
+++ app/src/main/java/com/jair/rdc216/fragments/frag_activit_main/Checked.java
@@ -25,6 +25,6 @@ public class Checked extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_checked, container, false);
+        return inflater.inflate(R.layout.fragment_check, container, false);
     }
 }
